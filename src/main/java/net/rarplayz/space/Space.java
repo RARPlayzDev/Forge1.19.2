c18ccededb92bfa9302bd4a67e92b2a339ff9331
RARPlayzDev <kaarthikeyanganesh@gmail.com>
2022-10-15 17:28:17 +0530

    Items1

--- src/main/java/net/rarplayz/space/Space.java
+++ src/main/java/net/rarplayz/space/Space.java
@@ -10,6 +10,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.rarplayz.space.item.ModItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -21,6 +22,7 @@ public class Space
     public Space()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        ModItems.register(modEventBus);
         modEventBus.addListener(this::commonSetup);
         MinecraftForge.EVENT_BUS.register(this);
     }
